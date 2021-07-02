6c121a39eff923d924bcebe14aafaf0ea119233e
YuanChenyc <36838244+YuanChenyc@users.noreply.github.com>
2021-07-02 15:39:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,19 @@
+
+from __future__ import print_function
+
+import boto3
 import json
-from flask import Flask, request
-app = Flask(__name__)
+import time
 
-@app.route('/event-invoke', methods = ['POST'])
-def invoke():
-    # Get all the HTTP headers from the official documentation of Tencent
-    request_id = request.headers.get("X-Scf-Request-Id", "")
-    print("SCF Invoke RequestId: " + request_id)
+print('Loading function')
 
-    event = request.get_data()
-    event_str = event.decode("utf-8")
 
-    print ("Hello from SCF event function, your input: ")
-    rep = {
-        "isBase64Encoded": "false",
-        "statusCode": "200",
-        "headers": {
-            "x-custom-header": "no"
-        },
-        "body": "Hello"
-    }
-    print ("Hello,World")
-    return json.dumps(rep)
-
-@app.route('/web-invoke/python-flask-http', methods = ['POST','GET'])
-def web_invoke():
-    # Get all the HTTP headers from the official documentation of Tencent
-    request_id = request.headers.get("X-Scf-Request-Id", "")
-    print("SCF Invoke RequestId: " + request_id)
-    
-    event = request.get_data()
-    event_str = event.decode("utf-8")
-    
-    print ("Hello from SCF HTTP function, your input: ")
-    rep = {
-        "isBase64Encoded": "false",
-        "statusCode": "200",
-        "headers": {
-            "x-custom-header": "no"
-        },
-        "body": "Hello"
-    }
-    print ("Hello,World")
-    return json.dumps(rep)
-
-@app.route('/invoke', methods = ['POST','GET'])
-def handler():
-    rep = {
-        "isBase64Encoded": "false",
-        "statusCode": "200",
-        "headers": {
-            "x-custom-header": "no"
-        },
-        "body": "hello"
-    }
-    return json.dumps(rep)
-
-def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    return { 
-        'message' : message
-    }
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+def handler(event, context):
+    '''Provide an event that contains the following keys:
+      - operation: one of the operations in the operations dict below
+      - tableName: required for operations that interact with DynamoDB
+      - payload: a parameter to pass to the operation being performed
+    '''
+    time.sleep(3)
+    print("Received event: " + json.dumps(event, indent=2))
+    return "Hello,World!!"
